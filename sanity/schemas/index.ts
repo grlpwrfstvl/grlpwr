608cb4ac3242cec11a4d2138d451a82263377ca6
HaaVad <123933986+HaaVad@users.noreply.github.com>
2023-12-19 14:38:26 +0100

    minor fix

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -4,7 +4,9 @@ import gallery from './gallery'
 import news from './news'
 import page from './page'
 import home from './home'
+import eventer from './eventer'
+import workshop from './workshop'
 
-const schemas = [artist, gallery, news, page, blockContent, home]
+const schemas = [artist, eventer, gallery, news, page, blockContent,workshop, home]
 
 export default schemas;
