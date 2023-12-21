a20361522d66386baaf0941c536e3fc25d6bde75
HaaVad <123933986+HaaVad@users.noreply.github.com>
2023-12-21 11:31:38 +0100

    Pic to page

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -1,23 +1,39 @@
 import { PortableText } from "@portabletext/react";
 import { getPage } from "../../../../sanity/sanity-utils";
+import Image from "next/image";
+import { blobPaths } from "../components/blobpaths";
 
-type Props = { 
-    params: { slug : string }
-}
+type Props = {
+  params: { slug: string };
+};
 
-export default async function Page ({params}: Props) {
-    const page = await getPage(params.slug);
+export default async function Page({ params }: Props) {
+  const page = await getPage(params.slug);
 
-    return (
-
-        <div className="max-w-5xl mx-auto">
-            <h1 className="pt-10 text-4xl text-gray-700 drop-shadow font-extrabold">
-                {page.title}
-            </h1> 
-
-            <div className="">
-                <PortableText value={page.content} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="max-w-4xl mx-auto">
+      <h1 className="pt-10 text-4xl text-gray-700 drop-shadow font-extrabold">
+        {page.title}
+      </h1>
+      <div>
+      <svg viewBox="5 0 400 400" className="absolute w-64 h-64 inset-0 z-20" fill="#fff">
+      <clipPath id={page._id}>
+      <path d={blobPaths[3]} transform="scale(0.85, 0.62)" />
+      </clipPath>
+      </svg>
+      <Image
+        src={page.image}
+        alt="GRL PWR logo"
+        priority={true}
+        height={400}
+        width={400}
+        className=""
+        style={{ clipPath: `url(#${page._id})` }}
+      />
+    </div>
+      <div className="">
+        <PortableText value={page.content} />
+      </div>
+    </div>
+  );
 }
--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -4,7 +4,14 @@ export default function Contact() {
       className=""
     >
       <div> 
-        will come
+        <h1>Kontakt</h1>
+       
+        <p>
+          Spille på GRLP PWR Festivalen? 
+          Send oss en mail med beskrivelse av prosjektet deres med linker. 
+          Vi tar konktakt dersom det blir aktuelt! 
+          Artister som har spilt på GRL PWR FSTVL 
+          No. 4, Blomst, Sløtface, Hudkreft, Pikekyss, Klossmajor, Of All Things, Alt kan Brennes Edvard, MONNE, Han Gaiden, Frøkedal og Familien m. flere. </p>
       </div>
     </main>
   )
