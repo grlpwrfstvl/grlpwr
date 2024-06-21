11ca6a34e4491f7383bbdd22685fcf54faabb5dc
grlpwrfstvl <150253724+grlpwrfstvl@users.noreply.github.com>
2024-06-21 14:54:55 +0200

    Update page.tsx

--- src/app/(site)/gallery/page.tsx
+++ src/app/(site)/gallery/page.tsx
@@ -15,20 +15,29 @@ function urlFor(source: any) {
 
   const images22 = galleries && galleries[0].images ? galleries[0].images.map(image => urlFor(image).url()) : [];
   const images23 = galleries && galleries[1].images ? galleries[1].images.map(image => urlFor(image).url()) : [];
+  const images24 = galleries && galleries[2].images ? galleries[2].images.map(image => urlFor(image).url()) : [];
+
+
 
 
     return (
         <main className="w-full py-2 md:py-20">
         <div className="flex flex-col justify-center">
         <h1 className="text-3xl md:text-5xl p-4 md:px-10 text-grlPink drop-shadow font-extrabold">
+            Bilder fra GRLPWR 24
+        </h1> 
+        <Carousel images={images24} />
+
+        <h1 className="text-3xl md:text-5xl pt-8 p-4 md:px-10 text-grlPink drop-shadow font-extrabold">
             Bilder fra GRLPWR 23
+        </h1> 
+        <Carousel images={images23} />
+
+        <h1 className="text-3xl md:text-5xl p-4 md:px-10 text-grlPink drop-shadow font-extrabold">
+            Bilder fra GRLPWR 22
         </h1> 
         <Carousel images={images22} />
 
-        <h1 className="text-3xl md:text-5xl pt-8 p-4 md:px-10 text-grlPink drop-shadow font-extrabold">
-            Bilder fra GRLPWR 22
-        </h1> 
-        <Carousel images={images23} />
 
           
         </div>
