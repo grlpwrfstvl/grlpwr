25e43f007b9dccb392f94f99e5d6d403e23ea57e
HaaVad <123933986+HaaVad@users.noreply.github.com>
2025-04-09 20:56:36 +0200

    minor fix

--- src/app/(site)/artists/[artist]/page.tsx
+++ src/app/(site)/artists/[artist]/page.tsx
@@ -25,30 +25,26 @@ export default async function Artist({ params }: any) {
   const spotiLink = artist.spotify;
 
   const nameLength = artist.name.length;
-  const nameSize = nameLength>16? "base" : "xl";
+  const nameSize = nameLength>18? "lg" : "xl";
 
   return (
     <main className="pl-0 md:pl-12">
-      <div className="relative flex flex-col w-full max-w-5xl md:flex-row">
+      <div className="relative flex flex-col w-full max-w-5xl mb-6 md:flex-row">
 
     <div className='z-10 pl-4 -mb-16 md:w-2/3 md:-mb-0 md:pl-0'>
     <ArtistPortrait key={artist._id} artist={artist} />
     </div>
-    {/* <div 
-     className='z-10 object-cover object-center -mb-10 opacity-100 md:opacity-0 w-100vw md:w-0'>
-      <ArtistPortraitSmall key={artist._id} artist={artist} index={1}  />
-    </div> */}
 
-    <div className="relative z-20 w-4/6 h-48 mt-0 md:w-2/6 md:mt-20 moving-object">
+    <div className="relative z-20 w-4/6 h-40 mt-0 md:w-2/6 md:mt-24 moving-object">
     <svg viewBox="0 0 500 500" className="absolute inset-0 z-10">
     <path d={blobPaths[2]} fill="#e82265" transform="scale(1.05)"/>
     </svg>
-    <div className="absolute z-30 w-3/4 pt-8 pl-4 text-base font-semibold font-bold text-white md:pt-12 top-1 md:pl-6 left-1 md:text-lg">
+    <div className="absolute z-30 w-3/4 pt-8 pl-6 text-base font-bold text-white md:pt-12 top-1 md:pl-6 left-1 md:text-lg">
     <h2 className={`text-${nameSize} md:text-2xl`}>{artist.name}</h2>
-    <h2>{formattedDate}</h2>
-    <h2>{formattedTime}</h2> 
+    <p>{formattedDate}</p>
+    <p>{formattedTime}</p> 
     {/* <h2>Tidspunkt: tba</h2> */}
-    <h2>{artist.stage}</h2>
+    <p>{artist.stage}</p>
     <div className="flex justify-center gap-8 p-2 md:p-5">
     {artist.instagram && (
     <a
@@ -78,7 +74,7 @@ export default async function Artist({ params }: any) {
   <svg viewBox="-10 0 400 350" className="absolute inset-0 opacity-0 md:opacity-100 -z-10 -mt-28">
     <path d={blobPaths[2]} fill="#e82265" transform="scale(0.96, 0.76)" />
   </svg>
-  <div className="relative z-10 w-full px-4 pt-10 mx-auto font-semibold md:w-3/4 md:px-0 text-grlPink md:text-white md:pb-40">
+  <div className="relative z-10 w-full px-4 mx-auto font-bold md:pt-10 md:w-3/4 md:px-0 text-grlPink md:text-white md:pb-40">
       <PortableText value={artist.description} />
   </div>
   </div>
